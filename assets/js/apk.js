708d9f5024f60a493e1d086d12a6761b2cebaed9
rezdiany <63382198+rezdiany@users.noreply.github.com>
2020-12-25 19:30:13 +0700

    Add files via upload

---
+++ assets/js/apk.js
@@ -0,0 +1,4 @@
+const text = document.querySelector(".changeColor")
+const changeColor = document.querySelector(".changeColor");
+changeColor.addEventListener("click",function() {
+	text.classlist.add("change");});
